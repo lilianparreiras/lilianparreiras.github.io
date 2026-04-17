dcea1e51096bd389d6c8b51c199f7d0a64093337
kukukaia <kukukaia@protonmail.com>
2026-04-17 10:12:14 -0300

    criação de botao para entrar no site

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const menu = document.querySelector('nav ul');
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+// Seleciona os elementos da splash screen
+const splash = document.getElementById('splash-screen');
+const btnEntrar = document.getElementById('entrar-site');
+
+if (btnEntrar) {
+    btnEntrar.addEventListener('click', () => {
+        splash.classList.add('hidden-splash');
+    });
+}
