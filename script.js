42d8ceffba075bde46142bdb71da4f2fb2efc2c7
kukukaia <kukukaia@protonmail.com>
2026-04-17 08:02:05 -0300

    testes

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ btnMenu.addEventListener('click', () => {
     // Adiciona ou remove a classe 'active' para mostrar/esconder o menu
     nav.classList.toggle('active');
 });
+
+btnMenu.addEventListener('click', () => {
+    console.log("O botão foi clicado!"); // Isso vai aparecer no console do F12
+    menuList.classList.toggle('active');
+});
