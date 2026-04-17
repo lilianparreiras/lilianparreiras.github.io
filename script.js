46e11afed1d79bf7b3225e2400287e43a7af79a9
kukukaia <kukukaia@protonmail.com>
2026-04-17 09:42:30 -0300

    corrigindo css

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Seleciona o botão do menu
-const btnMenu = document.querySelector('.menu-mobile');
-const nav = document.querySelector('.nav-links');
+const btn = document.querySelector('.menu-mobile');
+const menu = document.querySelector('nav ul');
 
-// Adiciona um "escutador" de clique
-btnMenu.addEventListener('click', () => {
-    // Adiciona ou remove a classe 'active' para mostrar/esconder o menu
-    nav.classList.toggle('active');
-});
-
-btnMenu.addEventListener('click', () => {
-    console.log("O botão foi clicado!"); // Isso vai aparecer no console do F12
-    menuList.classList.toggle('active');
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active');
 });
